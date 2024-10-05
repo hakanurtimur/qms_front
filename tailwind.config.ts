040059de7295b609ccde7f36ba6e4a7f1ad3aa01
Hakan Urtimur <dythakanurtimur@gmail.com>
2024-10-05 17:45:20 +0300

    Initial Commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,77 @@
 import type { Config } from "tailwindcss";
+import colors from "tailwindcss/colors";
 
-const config: Config = {
+const config = {
+  darkMode: ["class"],
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/modules/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/forms/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  prefix: "",
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: colors.blue,
+        secondary: colors.green,
+        tertiary: colors.yellow,
+        success: colors.green,
+        warning: colors.amber,
+        info: colors.cyan,
+        error: colors.red,
+        black: colors.gray,
+        background: colors.white,
+      },
+      keyframes: {
+        "accordion-down": {
+          from: {
+            height: "0",
+          },
+          to: {
+            height: "var(--radix-accordion-content-height)",
+          },
+        },
+        "accordion-up": {
+          from: {
+            height: "var(--radix-accordion-content-height)",
+          },
+          to: {
+            height: "0",
+          },
+        },
+        "slide-in-from-left": {
+          from: {
+            transform: "translateX(-100%)",
+          },
+          to: {
+            transform: "translateX(0)",
+          },
+        },
+        "slide-in-from-right": {
+          from: {
+            transform: "translateX(100%)",
+          },
+          to: {
+            transform: "translateX(0)",
+          },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+        "slide-in-from-left": "slide-in-from-left 0.55s ease-out",
+        "slide-in-from-right": "slide-in-from-right 0.55s ease-out",
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
       },
     },
   },
-  plugins: [],
-};
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config;
+
 export default config;
