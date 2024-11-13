28ee6f4320d6103adf9eb01984519d69982d8c4e
Shamkhal Huseynzade <shamkhalhuseynzade26@gmail.com>
2024-11-13 19:01:34 +0300

    logo comp değiştirildi

--- src/components/ui/Logo.tsx
+++ src/components/ui/Logo.tsx
@@ -1,11 +1,14 @@
+import { Cpu } from "lucide-react";
+
 interface Props {
   className?: string;
 }
 
 const Logo = ({ className }: Props) => {
   return (
-    <div className={"font-semibold text-3xl text-primary-900 " + className}>
-      QMS
+    <div className={"font-bold flex justify-center items-center text-3xl text-primary-900 " + className}>
+      <Cpu className="h-8 w-8 mr-2" />
+      Qubqa
     </div>
   );
 };
