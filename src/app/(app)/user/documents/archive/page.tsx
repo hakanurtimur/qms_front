80073604842eb53cd932d0f923bcaef1fa6b6315
Sebahattin <1030220637@erciyes.edu.tr>
2024-12-23 14:15:45 +0300

    Fix Subject

--- src/app/(app)/user/documents/archive/page.tsx
+++ src/app/(app)/user/documents/archive/page.tsx
@@ -1,6 +1,6 @@
 import PageBody from "@/components/page-body";
 import ArchiveContent from "@/app/(app)/user/documents/archive/_components/archive-content";
-/* not sheet'de kaydete basınca dökümanı pdf viewerı açıyor */
+/* not sheet kaydete basınca dökümanı pdf viewerı açıyor */
 const Page = () => {
   return (
     <PageBody>
